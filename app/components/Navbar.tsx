5c5bad9e45ea9c4abdf5f745e5c1e737d2e0097f
David Martínez <dmmtapia.ux@gmail.com>
2024-07-10 20:38:43 -0400

    navbar-logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Image from 'next/image'
+import Logo from "@/public/assets/logo-white.png"
 
 export const Navbar = () => {
   return (
-    <nav>
-        
+    <nav className='flex w-full item-center px-[20px] py-[16px]'>
+        <div>
+            <Image src={Logo} alt='Logo' width={180}/>
+        </div>
     </nav>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Navbar } from "./components/Navbar";
+
 export default function Home() {
   return (
-    <main>
-      <h1>hola mundo</h1>
-    </main>
+    <>
+      <Navbar/>
+    </>
   );
 }

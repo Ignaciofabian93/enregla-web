d26db71e484aa6e06e8cd41eccf201a9129effbd
David Martínez <dmmtapia.ux@gmail.com>
2024-07-10 22:48:03 -0400

    navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
 import Logo from "@/public/assets/logo-white.png"
+import { IconMenu } from './icons/IconMenu'
 
 export const Navbar = () => {
   return (
-    <nav className='flex w-full item-center px-[20px] py-[16px]'>
-        <div>
-            <Image src={Logo} alt='Logo' width={180}/>
+    <nav className='flex w-full justify-between px-[20px] py-[16px]'>
+        <div className='flex items-center'>
+            <Image src={Logo} alt='Logo' width={150}/>
         </div>
+
+<div className='text-[#9747FF]'>
+        <IconMenu fill='#fff'/>
+
+</div>
     </nav>
   )
 }
